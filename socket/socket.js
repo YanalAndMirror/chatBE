bf72ed9ea664f94bf903af5283b46f60717a776c
mirrorcall96 <mirrorcall96@gmail.com>
2021-07-31 01:42:49 +0300

    peer v0.1

--- socket/socket.js
+++ socket/socket.js
@@ -1,38 +1,29 @@
-const Session = require('../models/Session');
-const ChatMessages = require('./ChatMessage');
-const Disconnect = require('./Disconnect');
-const MessageUpdate = require('./MessageUpdate');
-const RoomSeen = require('./RoomSeen');
-const StartSession = require('./startSession');
+const Session = require("../models/Session");
+const ChatMessages = require("./ChatMessage");
+const Disconnect = require("./Disconnect");
+const MessageUpdate = require("./MessageUpdate");
+const RoomSeen = require("./RoomSeen");
+const StartSession = require("./startSession");
 
 const SocketIo = () => {
   // Clear sessions
   Session.deleteMany({ user: { $exists: true, $ne: null } }).then((a) => {});
-  io.on('connection', (socket) => {
-    socket.on('startSession', StartSession(socket));
-    socket.on('disconnect', Disconnect(socket));
-    socket.on('chatMessage', ChatMessages);
-    socket.on('roomSeen', RoomSeen);
-    // yanal trying
-    socket.emit('me', socket.id);
-    socket.on('end', () => {
-      socket.broadcast.emit('callEnded');
+  io.on("connection", (socket) => {
+    socket.on("startSession", StartSession(socket));
+    socket.on("disconnect", Disconnect(socket));
+    socket.on("chatMessage", ChatMessages);
+    socket.on("roomSeen", RoomSeen);
+    socket.on("messageUpdate", MessageUpdate);
+    socket.on("peer", async ({ data, userId }) => {
+      let thisUser = await Session.find({ user: userId });
+      console.log("peer" + thisUser);
+      socket.to(thisUser.map((a) => a.socket)).emit("startPeer", { data });
     });
-    socket.on('callUser', (data) => {
-      io.to(data.userToCall).emit('callUser', {
-        signal: data.signalData,
-        from: data.from,
-        name: data.name,
-      });
+    socket.on("peerRecive", async ({ data, userId }) => {
+      let thisUser = await Session.find({ user: userId });
+      console.log("peerRecive" + thisUser);
+      socket.to(thisUser.map((a) => a.socket)).emit("reciverPeer", { data });
     });
-    socket.on('answerCall', (data) => {
-      io.to(data.to).emit('callAccepted', data.signal);
-    });
-    //
-    // Delete for all / Edit
-    socket.on('messageUpdate', MessageUpdate);
-
-    //
   });
 };
 module.exports = SocketIo;
